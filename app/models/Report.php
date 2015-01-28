e05cab3f2195574bf8d3cd654aecb7c27819f7c8
Marcio <marcioatlondon@gmail.com>
2015-01-28 20:28:17 +0000

    Added reports resource

---
+++ app/models/Report.php
@@ -0,0 +1,17 @@
+<?php
+
+class Report extends \Eloquent {
+
+	use SoftDeletingTrait;
+
+	// Add your validation rules here
+	public static $rules = [
+		'report_date' => 'required'
+	];
+
+	// Don't forget to fill this array
+	protected $fillable = [
+		'report_date'
+	];
+
+}
