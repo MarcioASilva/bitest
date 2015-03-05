3c4fd29e14aa2ae2c85ea26f5afd22a273e20c7c
Marcio <marcioatlondon@gmail.com>
2015-03-05 21:56:07 +0000

    a

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -2,16 +2,51 @@
 
 class PagesController extends Controller {
 
+
+  public function getSelectDropdown()
+  {
+    // $selectDrodpdown = ExportedDate::orderBy('exported_date', 'desc')->exported_date;
+
+    $selectDrodpdown = ExportedDate::orderBy('exported_date')->get();
+
+    $trimmedDropdown = [];
+
+    foreach($selectDrodpdown as $row)
+    {
+      // $row->exported_date = Carbon::createFromFormat('Y-m-d H:i:s', $row->exported_date)->format('jS F Y');
+      $row->exported_date = Carbon::createFromTimeStamp(strtotime($row->exported_date))->format('jS F Y');
+
+      $trimmedDropdown[] = $row;
+    }
+
+    // includes get
+    // all();
+    // first();
+    // find();
+
+    // does not include get
+    // orderBy()
+    // where()
+
+
+    return Response::json([
+        'error'   => false,
+        'records' => $trimmedDropdown
+      ],
+      200
+    );
+  }
+
   //Data for Cover page
   // /api/charts/cover-page
   public function getCoverPage()
   {
 
     $exportedDate = ExportedDate::orderBy('exported_date', 'desc')->first()->exported_date;
-    $reportDate   = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $exportedDate)
+    $reportDate   = Carbon::createFromFormat('Y-m-d H:i:s', $exportedDate)
       ->subMonth()
       ->lastOfMonth()
-      ->format('jS F Y'); 
+      ->format('jS F Y');
 
     return Response::json([
         'error'   => false,
