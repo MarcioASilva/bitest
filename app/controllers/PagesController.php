229e06a918060ab46e12c3b392b6eefeac83d5df
Marcio <marcioatlondon@gmail.com>
2015-03-05 01:26:53 +0000

    Changes to contolle

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -2,20 +2,37 @@
 
 class PagesController extends Controller {
 
-  // /api/charts/volume-summary-table
+  //Data for Cover page
+  // /api/charts/cover-page
+  public function getCoverPage()
+  {
+
+    $exportedDate = ExportedDate::orderBy('exported_date', 'desc')->first()->exported_date;
+    $reportDate   = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $exportedDate)
+      ->subMonth()
+      ->lastOfMonth()
+      ->format('jS F Y'); 
+
+    return Response::json([
+        'error'   => false,
+        'records' => [
+          'exported_date' => strtotime($exportedDate),
+          'report_date'   => $reportDate
+        ]
+      ],
+      200
+    );
+  }
+
+  // /api/charts/page1
   public function getPage1() //VolumeSummaryTable
   {
     $volumeSummaryTable = Record::all();
 
-
-
-    //Record::;
-
-
-    return Response::json(array(
-        'error' => false,
+    return Response::json([
+        'error'   => false,
         'records' => $volumeSummaryTable->toArray()
-      ),
+      ],
       200
     );
   }
