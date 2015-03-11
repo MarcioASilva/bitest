fe2fdb45cf9eeeecfbee6551ac0328e23e564fe9
Marcio <marcioatlondon@gmail.com>
2015-03-11 02:48:18 +0000

    Dataset from spreadsheet and DB working

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -11,12 +11,12 @@ class PagesController extends Controller {
 
     $trimmedDropdown = [];
 
-    foreach($selectDrodpdown as $row)
+    foreach($selectDrodpdown as $rows)
     {
-      // $row->exported_date = Carbon::createFromFormat('Y-m-d H:i:s', $row->exported_date)->format('jS F Y');
-      $row->exported_date = Carbon::createFromTimeStamp(strtotime($row->exported_date))->format('F Y');
+      // $rows->exported_date = Carbon::createFromFormat('Y-m-d H:i:s', $rows->exported_date)->format('jS F Y');
+      $rows->exported_date = Carbon::createFromTimeStamp(strtotime($rows->exported_date))->format('F Y');
 
-      $trimmedDropdown[] = $row;
+      $trimmedDropdown[] = $rows;
     }
 
     // includes get
@@ -72,19 +72,85 @@ class PagesController extends Controller {
     );
   }
 
+  // public function getImportFile()
+  // {
+
+  //   $spreadsheet = Excel::load(public_path() . '/uploads/file_1k.csv')->get();
+
+  //   foreach ($spreadsheet as $rows) {
+  //     Dataset::create([
+  //       'dataset'=> $rows->dataset
+  //     ]);
+  //   }
+
+  //   var_dump('dataset entered');
+
+  // }
+
   public function getImportFile()
   {
 
-    $results = Excel::load(public_path() . '/uploads/file_1k.csv')->get();
+    //Imports spreadsheet into $spreadsheet
+    $spreadsheet = Excel::load(public_path() . '/uploads/file_1k.csv')->get();
+    
+    $spreadsheetDataset[]='';
+    $counter1=0;
 
-    foreach ($results as $row) {
-      Dataset::create([
-        'dataset'=> $row->dataset
-      ]);
+    //puts dataset from $rows in to $spreadsheetDataset array
+    foreach ($spreadsheet as $rows) {
+      $spreadsheetDataset[$counter1] = $rows ->dataset.'<br>';
+      $counter1++;
+      // $rows = $rows ->toArray();
+      // print $rows.'<br>';
+      // echo $rows;
     }
 
-    var_dump('dataset entered');
+    //remove duplicates Aarays
+    $spreadsheetDataset = array_unique($spreadsheetDataset);
+    // print_r($spreadsheetDataset);
+    
+    echo 'Resultes from Spreadsheet: '.'<br>';
+    foreach ($spreadsheetDataset as $key => $value) {
+      echo $value;
+    }
 
+
+
+    //Dataset from DB
+    $DB = Dataset::all();
+    
+    $DBDataset[]='';
+    $counter2=0;
+
+    //puts dataset from $rows in to $spreadsheetDataset array
+    foreach ($DB as $rows) {
+      $DBDataset[$counter2] = $rows ->dataset.'<br>';
+      $counter2++;
+      // $rows = $rows ->toArray();
+      // print $rows.'<br>';
+      // echo $rows;
+    }
+
+    //remove duplicates Aarays
+    $DBDataset = array_unique($DBDataset);
+
+    //echo $DB;
+
+    echo '<br>'.'Resultes from DB: '.'<br>';
+    foreach ($DBDataset as $key => $value) {
+      echo $value;
+    }
+    // var_dump($DB);
+
+    //Compare results from DB x Spreadsheet
+
+    //call comparing function 
   }
 
+  public function CompareArrays(array1, array2)
+  {
+    foreach ($variable as $key => $value) {
+      # code...
+    }
+  }
 }
