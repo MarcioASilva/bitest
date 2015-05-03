377dea2b3499d7438a2d4c50918ac03f5f09d171
Marcio <marcioatlondon@gmail.com>
2015-05-03 16:51:04 +0100

    Not even sure

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  =>  $_SERVER[DATABASE1_NAME],
-			'username'  =>  $_SERVER[DATABASE1_USER],
-			'password'  =>  $_SERVER[DATABASE1_PASS],
+			'host'      => $_SERVER['DATABASE1_HOST'],
+			'database'  => $_SERVER['DATABASE1_NAME'],
+			'username'  => $_SERVER['DATABASE1_USER'],
+			'password'  => $_SERVER['DATABASE1_PASS'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
